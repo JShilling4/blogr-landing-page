ede11ea5aa17c32b2342377e567ad7198cd812f2
Justin Shilling <loveofmusic25@hotmail.com>
2021-05-13 19:55:49 -0400

    added footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,87 @@
+<template>
+    <footer class="footer">
+        <div class="outer-container">
+            <div class="logo-container">
+                <img src="@/assets/images/logo.svg" alt="Blogr logo">
+            </div>
+            
+            <div class="footer-nav">
+                <ul>
+                    <li>Product</li>
+                    <li>Overview</li>
+                    <li>Pricing</li>
+                    <li>Marketplace</li>
+                    <li>Features</li>
+                    <li>Integrations</li>
+                </ul>
+
+                <ul>
+                    <li>Company</li>
+                    <li>About</li>
+                    <li>Team</li>
+                    <li>Blog</li>
+                    <li>Careers</li>
+                </ul>
+
+                <ul>
+                    <li>Connect</li>
+                    <li>Contact</li>
+                    <li>Newsletter</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "TheFooter",
+    }
+</script>
+
+<style lang="scss" scoped>
+.footer {
+    margin-top: 10rem;
+    height: 45rem;
+    padding: 10rem 0;
+    background-color: $footer-bg-color;
+    border-top-right-radius: 12rem;
+
+    .outer-container {
+        display: flex;
+    }
+
+    .logo-container {
+        width: 30%;
+        img {
+            width: 12rem;
+        }
+    }
+
+    .footer-nav {
+        display: flex;
+        justify-content: space-between;
+        width: 55%;
+        color: $footer-text-color;
+        li {
+            font-size: 2rem;
+            
+        }
+        li:first-of-type {
+            font-weight: 600;
+            margin-bottom: 3rem;
+        }
+        li:not(:first-of-type) {
+            color: darken($footer-text-color, 15%);
+            padding: .75rem 0;
+            cursor: pointer;
+            &:hover {
+                text-decoration: underline;
+                
+            }
+        }
+    }
+}
+
+</style>
